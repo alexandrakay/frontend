3c07251bc66fc03d66ff9ad2eb9c7cfd2a6c3156
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-11-09 22:10:28 -0500

    nav token

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./NavBar.css"
+import "./NavBar.css";
 
-const token = localStorage.getItem("token")
+const token = localStorage.getItem("token");
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      {token  &&
-      <Link to="/comments">Comments</Link>
-      }
-      {token  &&
-      <Link to="/logout">Logout</Link>
-      }
+      {token && <Link to="/">Home</Link>}
+      {token && <Link to="/comments">Comments</Link>}
+      {token && <Link to="/logout">Logout</Link>}
 
       {/* <Link to="/landingpage">Marketing</Link> */}
-     
-
     </nav>
   );
 }
